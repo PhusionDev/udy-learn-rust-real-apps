8ecfbc7302c9356d656170602ddb7352928eaf48
Jared Hodgkins <jared.hodgkins@gmail.com>
2024-02-08 15:30:40 -0500

    removed build files from  tracking

--- server/target/debug/deps/server-449e4c3a90de55c5.d
+++
@@ -1,9 +0,0 @@
-/Users/jaredhodgkins/Projects/rust/udy-learn-rust-real-apps/server/target/debug/deps/server-449e4c3a90de55c5: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
-
-/Users/jaredhodgkins/Projects/rust/udy-learn-rust-real-apps/server/target/debug/deps/server-449e4c3a90de55c5.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
-
-src/main.rs:
-src/http/mod.rs:
-src/http/method.rs:
-src/http/request.rs:
-src/server.rs:
--- server/target/debug/deps/server-512e4f566cbccd35.d
+++
@@ -1,9 +0,0 @@
-/Users/jaredhodgkins/Projects/rust/udy-learn-rust-real-apps/server/target/debug/deps/libserver-512e4f566cbccd35.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
-
-/Users/jaredhodgkins/Projects/rust/udy-learn-rust-real-apps/server/target/debug/deps/server-512e4f566cbccd35.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/server.rs
-
-src/main.rs:
-src/http/mod.rs:
-src/http/method.rs:
-src/http/request.rs:
-src/server.rs:
